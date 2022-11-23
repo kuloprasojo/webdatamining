66c9a355a0f645635e1b9e73316be9e142a1a597
kuloprasojo <112039203+kuloprasojo@users.noreply.github.com>
2022-11-24 01:07:26 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -18,12 +18,6 @@ from sklearn.datasets import make_classification
 from sklearn.svm import SVC
 import altair as alt
 from sklearn.utils.validation import joblib
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt # plotting
-import numpy as np # linear algebra
-import os # accessing directory structure
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 
 
